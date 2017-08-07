c0a8a0d70bdc4814889a5a1f10f9de13de9da64b
Kimberly Luu <kimzillacode@gmail.com>
2017-08-06 23:09:15 -0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+/*
+ * Creates the document using the model json
+ *
+ * Example:
+ * Name of patient: Jane Doe
+ * Organization name: College Hospital
+ * Gender: Female
+ * Number of conditions they have: 2
+ * List of all conditions:
+ * - Epilepsy
+ * - Sleep apnea
+ */
+ console.log('hello')
+
